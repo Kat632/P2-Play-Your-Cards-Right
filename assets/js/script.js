14aad36266c267b3635059034013d510d3a97eb8
Kat632 <katherine.biggs@mail.com>
2021-10-07 16:43:20 +0000

    build the cards, create loop to get the cards in javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 let myQuids = 1000;
 
+//cards
+let cards = [];
+let suits = ["spades","hearts","clubs","diams"];
+let numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+
+
 let rules = document.getElementById('rules');
 
 
@@ -8,4 +14,19 @@ let rules = document.getElementById('rules');
 function gameStart() {
   rules.innerHTML = "Game Started!";
   document.getElementById('start').style.display = 'none';
+  buildCards();
+}
+//build the cards - loop through the card arrays
+function buildCards() {
+    cards = [];
+    for(s in suits) {
+        for (n in numbers) {
+            let card = {
+                suit : suits[s]
+                , num : numbers[n]
+            }
+            cards.push(card);
+        }
+    }
+    console.log(cards);
 }
