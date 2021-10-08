34e1449d25d519fa27322e098de67bc359fd9f80
Kat632 <katherine.biggs@mail.com>
2021-10-08 10:53:04 +0000

    as per previous push, did not save the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ let myQuids = 1000;
 
 //cards
 let cards = [];
-let suits = ["spades","hearts","clubs","diams"];
-let numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+let suits = ["spades", "hearts", "clubs", "diams"];
+let numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let count = 0;
 let cardOutput = document.getElementById('cards');
 
@@ -13,33 +13,36 @@ let rules = document.getElementById('rules');
 
 
 function gameStart() {
-  rules.innerHTML = "Game Started!";
-  document.getElementById('start').style.display = 'none';
-  document.getElementById('highLow').style.display = 'block';
-  buildCards();
-  shuffleArray(cards);
-  cardOutput.innerHTML += showCard();
+    rules.innerHTML = "Game Started!";
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('highLow').style.display = 'block';
+    buildCards();
+    shuffleArray(cards);
+    cardOutput.innerHTML += showCard();
 }
 
 function hilo(a) {
-  //calculate winner
-  let win = false;
-  let oldCard = cards[count].cardValue;
-  count++;
-  cardOutput.innerHTML += showCard();
-  let newCard = cards[count].cardValue;
-  if(a == 'high' && oldCard < newCard){win=true;}
-  else if (a == 'low' && oldCard > newCard){win=true;}
-  if(win) {
-    rules.innerHTML = "You were right!";
-  } else {
-    rules.innerHTML = "You were wrong!";
-  }
+    //calculate winner
+    let win = false;
+    let oldCard = cards[count].cardValue;
+    count++;
+    cardOutput.innerHTML += showCard();
+    let newCard = cards[count].cardValue;
+    if (a == 'high' && oldCard < newCard) {
+        win = true;
+    } else if (a == 'low' && oldCard > newCard) {
+        win = true;
+    }
+    if (win) {
+        rules.innerHTML = "You were right!";
+    } else {
+        rules.innerHTML = "You were wrong!";
+    }
 }
 
 //randomise the cards
 function shuffleArray(array) {
-    for (let i = array.length-1; i > 0; i--) {
+    for (let i = array.length - 1; i > 0; i--) {
         let holder = Math.floor(Math.random() * (i + 1));
         let temp = array[i];
         array[i] = array[holder];
@@ -51,8 +54,8 @@ function shuffleArray(array) {
 //display the cards to the user
 function showCard() {
     let c = cards[count];
-    let bgColor = (c.icon == "H" || c.icon == "D") ? 'red' : 'black';  //Get colours for the suits
-    return '<span class="icard" style="color:'+bgColor+'">'+c.num+'&'+c.suit+';</span>'; //Show the colours to the user
+    let bgColor = (c.icon == "H" || c.icon == "D") ? 'red' : 'black'; //Get colours for the suits
+    return '<span class="icard" style="color:' + bgColor + '">' + c.num + '&' + c.suit + ';</span>'; //Show the colours to the user
 }
 
 
@@ -63,14 +66,14 @@ function showCard() {
 //build the cards - loop through the card arrays
 function buildCards() {
     cards = [];
-    for(s in suits) {
+    for (s in suits) {
         let suit = suits[s][0].toUpperCase();
         for (n in numbers) {
             let card = {
-                suit : suits[s]
-                , num : numbers[n]
-                , cardValue : parseInt(n) + 2
-                , icon : suit
+                suit: suits[s],
+                num: numbers[n],
+                cardValue: parseInt(n) + 2,
+                icon: suit
             }
             cards.push(card);
         }
